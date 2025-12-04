a8cf86ede28dbbb1bec67a9e23464b1e78a51a3a
iOmega8561 <giusepperocco38@gmail.com>
2025-12-05 00:16:00 +0100

    Implemented authentication callbacks - VNC Password authentication is handled by client_password_callback - Username + Password is handled by client_credential_callback

--- EasyVNC/LibVNC/Callbacks.mm
+++ EasyVNC/LibVNC/Callbacks.mm
@@ -36,9 +36,13 @@
 
 #pragma clang diagnostic pop
 
+// This variable is never assigned. We are going
+// to use its address as a rfbClient content TAG.
 int kVNCClientTag;
 
-// Callback used when the server requests a new frame buffer allocation.
+// MARK: - Remote Framebuffer Resize Callback
+// Triggered on a new frame buffer allocation request.
+
 rfbBool resize_callback(rfbClient *cl) {
     
     // Free any existing framebuffer.
@@ -55,33 +59,40 @@ rfbBool resize_callback(rfbClient *cl) {
     
     if (!cl->frameBuffer) { return FALSE; }
 
-    // Making sure we don't catch dirty memory
+    // Making sure we don't catch dirty memory.
     memset(cl->frameBuffer, 0, size);
     
     return TRUE;
 }
 
-// Callback triggered when there is a framebuffer update.
+// MARK: - Remote Framebuffer Update Callback
+// Triggered on a framebuffer update.
+
 void framebuffer_update_callback(rfbClient *cl, int x, int y, int w, int h) {
     
     ClientWrapper *wrapper = (__bridge ClientWrapper *) rfbClientGetClientData(cl, &kVNCClientTag);
     
-    if (wrapper.delegate && cl->frameBuffer) {
-        
-        [wrapper.delegate didUpdateFramebuffer:(const uint8_t *)cl->frameBuffer
-                                         width:cl->width
-                                        height:cl->height
-                                        stride:(cl->width * (cl->format.bitsPerPixel / 8))];
+    // Basic NULL checks.
+    // There's no point in moving forward if these fail.
+    if (!wrapper || !wrapper.delegate || !cl->frameBuffer) {
+        return;
     }
+    // Safe enough to call the wrapper delegate's method.
+    [wrapper.delegate didUpdateFramebuffer:(const uint8_t *)cl->frameBuffer
+                                     width:cl->width
+                                    height:cl->height
+                                    stride:(cl->width * (cl->format.bitsPerPixel / 8))];
 }
 
-// Callback used by libVNCClient instead of default logging utility
+// MARK: - libVNCClient Logging Facility
+// Replaces libVNCClient's default logging utility.
+
 void client_log_callback(const char *format, ...) {
     va_list args;
     va_start(args, format);
 
     char buffer[2048];
-    // vsnprintf can return len > sizeof(buffer)
+    // vsnprintf can return len > sizeof(buffer).
     int len = vsnprintf(buffer, sizeof(buffer), format, args);
     va_end(args);
 
@@ -94,7 +105,62 @@ void client_log_callback(const char *format, ...) {
         buffer[len] = '\0';
     }
     
-    // Converting to NSData enhances flexibility in thi environment
+    // Converting to NSData enhances flexibility in this environment.
     NSData *data = [NSData dataWithBytes:buffer length:len];
     [[ClientLogger shared] writeLogData:data];
 }
+
+// MARK: - libVNCClient GetPassword Facility
+// Replaces libVNCClient's default method for password-only authentication.
+
+char* client_password_callback(rfbClient *cl) {
+    
+    ClientWrapper *wrapper = (__bridge ClientWrapper *)rfbClientGetClientData(cl, &kVNCClientTag);
+    
+    // Basic NULL checks
+    if (!wrapper || !wrapper.connection)
+        return NULL;
+    
+    return strdup([[wrapper.connection password] UTF8String]);
+}
+
+// MARK: - libVNCClient GetCredential Facility
+// Replaces libVNCClient's default method for credential authentication.
+
+rfbCredential* client_credential_callback(rfbClient* cl, int credentialType) {
+        
+    // There's no point in moving forward if this fails.
+    if (credentialType != rfbCredentialTypeUser) {
+        rfbClientErr("Authentication does not require USERNAME\n");
+        return NULL;
+    }
+    
+    // This needs to be checked for NULL first, segfault can occurr.
+    rfbCredential *credential = (rfbCredential *)malloc(sizeof(rfbCredential));
+    if (!credential) { return NULL; }
+    
+    // This we're gonna check later ...
+    credential->userCredential.username = (char *)malloc(RFB_BUF_SIZE);
+    credential->userCredential.password = (char *)malloc(RFB_BUF_SIZE);
+    // This too, look below for NULL checks ...
+    ClientWrapper *wrapper = (__bridge ClientWrapper *)rfbClientGetClientData(cl, &kVNCClientTag);
+    
+    // Now we can group these NULL checks, instead of having
+    // a bazillion of if-statements in this callback function.
+    if (!credential->userCredential.username ||
+        !credential->userCredential.password ||
+        !wrapper ||
+        !wrapper.connection
+    ) {
+        free(credential->userCredential.username);
+        free(credential->userCredential.password);
+        free(credential);
+        return NULL;
+    }
+
+    // Dupe the strings and send back to the client. Source is NSString, empty is fine.
+    credential->userCredential.username = strdup([[wrapper.connection username] UTF8String]);
+    credential->userCredential.password = strdup([[wrapper.connection password] UTF8String]);
+    return credential;
+}
+ 
